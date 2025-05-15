25c8d0602a0debe78771636e3e42a7486223b93f
Harry <harrylippa@gmail.com>
2025-05-15 09:29:43 +0100

    Added to the Auth and UseFetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(mediaFunction, initalDataValue, arg){
+    const [data, setData] = useState(initalDataValue)
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const { data } = await mediaFunction(arg)
+                setData(data)
+            } catch {
+                setError('Failed to fetch data. Please try again later.')
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        fetchData()
+    }, [mediaFunction, arg])
+
+    return { data, isLoading, error}
+}
