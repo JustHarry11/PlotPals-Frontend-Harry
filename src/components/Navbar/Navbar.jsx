09c7a2cfd17fb8efccf859beb62861b0be670f16
Harry <harrylippa@gmail.com>
2025-05-21 12:13:38 +0100

    Made changes to the profile content so you only see if you mouse over

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router";
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import { removeToken } from "../../utils/auth";
 import './Navbar.css'
@@ -9,69 +9,84 @@ export default function NavBar() {
     const [userSettings, setUserSettings] = useState(false)
     const { user, setUser } = useContext(UserContext)
 
+    const hoverProfile = useRef(false)
+    const hoverContent = useRef(false)
+
+    const openDropdown = () => setUserSettings(true)
+
+    const handleMouseLeave = () => {
+        setTimeout(() => {
+            if (!hoverProfile.current && !hoverContent.current) {
+                setUserSettings(false)
+            }
+        }, 100)
+    }
+
     const handleSignOut = () => {
         removeToken()
         setUser(null)
     }
 
-    function toggleDropdown() {
-        setUserSettings(drop => !drop)
-    }
-
-    function closeDropdown() {
-        setUserSettings(false)
-    }
 
     return (
         <header>
             <div className="top-navigation">
                 <div className="nav-left">
                     <div className="brand-logo">
-                        <NavLink to="/home" onClick={closeDropdown}>🎥</NavLink>
+                        <NavLink to="/home">🎥</NavLink>
                     </div>
-                    <NavLink to="/medias" onClick={closeDropdown}>Media</NavLink>
-                    <NavLink to="/genres" onClick={closeDropdown}>Genre</NavLink>
+                    <NavLink to="/medias">Media</NavLink>
+                    <NavLink to="/genres">Genre</NavLink>
                 </div>
                 <div className="nav-middle">
-                    <NavLink to="/home" onClick={closeDropdown}>
-                    <h1>PlotPals</h1>
+                    <NavLink to="/home">
+                        <h1>PlotPals</h1>
                     </NavLink>
                 </div>
-                    <nav className="nav-right">
-                        {user
-                            ? (
-                                <>
-                                <div  className="profile-dropdown">
-                                    <div onMouseEnter={toggleDropdown} className="username-toggle">
-                                        { user.username.charAt(0).toUpperCase() + user.username.slice(1) }
+                <nav className="nav-right">
+                    {user
+                        ? (
+                            <>
+                                <div className="profile-dropdown">
+                                    <div className="username-toggle" 
+                                        onMouseEnter={() => {hoverProfile.current = true; openDropdown()}}
+                                        onMouseLeave={() => {hoverProfile.current = false; handleMouseLeave()}}>
+                                        {user.username.charAt(0).toUpperCase() + user.username.slice(1)}
                                     </div>
                                     {userSettings && (
-                                    <div onMouseLeave={toggleDropdown} className="dropdown-content">
-                                        <NavLink to="/medias/new" onClick={closeDropdown}>Add Media</NavLink>
-                                        <NavLink to="/favourites" onClick={closeDropdown}>Your Favourites</NavLink>
-                                        <NavLink onClick={() => {handleSignOut(); closeDropdown();}} to="/login">Sign Out</NavLink>
-                                    </div>
+                                        <div className="dropdown-content"
+                                            onMouseEnter={() => (hoverContent.current = true)}
+                                            onMouseLeave={() => { hoverContent.current = false; handleMouseLeave()}}>
+                                            <NavLink to="/medias/new" >Add Media</NavLink>
+                                            <NavLink to="/favourites" >Your Favourites</NavLink>
+                                            <NavLink onClick={() => { handleSignOut() }} to="/login">Sign Out</NavLink>
+                                        </div>
                                     )}
-
                                 </div>
-                                </>
-                            )
-                            : (
-                                <>
-                                    <div onMouseEnter={toggleDropdown}  className="username-toggle">
+                            </>
+                        )
+                        : (
+                            <>
+                                <div className="profile-dropdown">
+                                    <div className="username-toggle"
+                                        onMouseEnter={() => { hoverProfile.current = true; openDropdown(); }}
+                                        onMouseLeave={() => { hoverProfile.current = false; handleMouseLeave() }}>
                                         Account
                                     </div>
                                     {userSettings && (
-                                    <div onMouseLeave={closeDropdown} className="dropdown-content">
-                                        <NavLink to="/register" onClick={closeDropdown}>Register</NavLink>
-                                        <NavLink to="/login" onClick={closeDropdown}>Log In</NavLink>
-                                    </div>
+                                        <div className="dropdown-content"
+                                            onMouseEnter={() => (hoverContent.current = true)}
+                                            onMouseLeave={() => { hoverContent.current = false; handleMouseLeave() }}>
+                                            <NavLink to="/register">Register</NavLink>
+                                            <NavLink to="/login">Log In</NavLink>
+                                        </div>
                                     )}
-                                </>
+                                </div>
+                            </>
 
-                            )
-                        }
-                    </nav>
+                        )
+                    }
+                </nav>
             </div>
         </header>
     )
